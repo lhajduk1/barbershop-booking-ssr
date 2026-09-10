a0d257b57dcd73c572f86c3b6661dd243e50f5bf
Łukasz <l.hajduk@dnavi.pl>
2026-09-10 13:19:16 +0200

    feat: add login redirect if user is admin

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Auth;
 
+use App\Enums\UserRole;
 use App\Http\Requests\Auth\LoginUserRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
@@ -15,8 +16,13 @@ final class LoginController
         if (Auth::attempt($request->safe()->only('email', 'password'), $request->filled('remember'))) {
             $request->session()->regenerate();
 
+            if ($request->user()->hasRole(UserRole::ADMIN)) {
+                return redirect()->intended('/admin');
+            };
+
             return redirect()->intended('/dashboard');
         }
+
 
         return back()->withErrors([
             'email' => 'The provided credentials do not match our records.',
